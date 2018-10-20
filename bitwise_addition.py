84d6550a3951d794500a89e80cfe48641baedbf4
magdapoppins <mmbste@utu.fi>
2018-10-20 14:13:47 +0300

    Add sketches for addition samples in python.

---
+++ bitwise_addition.py
@@ -0,0 +1,17 @@
+def bitwise_add(a, b):
+    # What to do if two arrays of not equal length?
+    if (len(a) != len(b)):
+        return "No way"
+
+    answer = []
+    for i in range(len(a)):
+        # Xor from left to right
+        # add result to answer
+
+    return answer
+
+
+
+
+
+        
---
+++ integer_addition.py
@@ -0,0 +1,15 @@
+carry = 0
+
+def integer_add(a, b):
+    # What to do if two arrays of not equal length?
+    if (len(a) != len(b)):
+        return "No way"
+    
+    answer = []
+    for i in range(len(a)):
+        # If 0 and 1 just add 1 then check carrier
+        if ()
+
+def use_carry(bit_value):
+    # If 0, just return 1 and set carrier to 0
+    # If 1 return 0 and reset carrier to 1
